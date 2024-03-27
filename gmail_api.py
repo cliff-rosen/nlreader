361dbb195b3e470b2092653f2133caae83af80b0
Cliff Rosen <cliff.rosen@gmail.com>
2024-03-27 05:07:43 -0400

    backup

--- gmail_api.py
+++ gmail_api.py
@@ -4,29 +4,61 @@ from google.auth.transport.requests import Request
 import os, pickle
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-CREDENTIALS_FILE = 'secrets/client_secret_604005571-hu893qnfe3nns3rj4uvc4a0clgji88da.apps.googleusercontent.com.json'
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+CREDENTIALS_FILE = "secrets/client_secret_604005571-hu893qnfe3nns3rj4uvc4a0clgji88da.apps.googleusercontent.com.json"
+
 
 def get_service():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('secrets/token.pickle'):
-        with open('secrets/token.pickle', 'rb') as token:
+    if os.path.exists("secrets/token.pickle"):
+        with open("secrets/token.pickle", "rb") as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CREDENTIALS_FILE, SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('secrets/token.pickle', 'wb') as token:
+        with open("secrets/token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
-    service = build('gmail', 'v1', credentials=creds)
+    service = build("gmail", "v1", credentials=creds)
     return service
 
+
+def get_messages(service, label, start_date, end_date):
+    query = f"after:{start_date} before:{end_date}"
+    results = (
+        service.users()
+        .messages()
+        .list(userId="me", labelIds=[label], maxResults=100, q=query)
+        .execute()
+    )
+    return results.get("messages", [])
+
+
+def get_message(service, id):
+    msg = (
+        service.users()
+        .messages()
+        .get(
+            userId="me",
+            id=id,
+            format="metadata",
+            metadataHeaders=["Subject"],
+        )
+        .execute()
+    )
+    return msg
+
+
+def get_labels():
+    service = get_service()
+    labels = service.users().labels().list(userId="me").execute().get("labels", [])
+
+    return labels
--- run.py
+++ run.py
@@ -1,32 +1,25 @@
-from gmail_api import get_service
+from gmail_api import get_service, get_labels, get_messages, get_message
 
-
-def get_labels():
-    service = get_service()
-    labels = service.users().labels().list(userId='me').execute().get('labels', [])
-
-    print("about to print")
-    for label in labels:
-        print(label)
 
 def main():
     service = get_service()
+    label = "Label_2689345936544895630"
+    start_date = "2024/03/25"
+    end_date = "2024/03/26"
+    messages = get_messages(service, label, start_date, end_date)
 
-    # get_labels()
-    # return
-
-    label = 'Label_2689345936544895630'
-    results = service.users().messages().list(userId='me', labelIds=[label], maxResults=5).execute()
-    messages = results.get('messages', [])
-
-   
     if not messages:
         print("No messages found.")
 
     for message in messages:
-        msg = service.users().messages().get(userId='me', id=message['id'], format='metadata', metadataHeaders=['Subject']).execute()
-        headers = msg.get('payload', {}).get('headers', [])
-        subject = next((header['value'] for header in headers if header['name'] == 'Subject'), 'No Subject')
+        msg = get_message(service, message["id"])
+        headers = msg.get("payload", {}).get("headers", [])
+        print(headers)
+        subject = next(
+            (header["value"] for header in headers if header["name"] == "Subject"),
+            "No Subject",
+        )
         print(f"Message ID: {message['id']}, Subject: {subject}")
-          
+
+
 main()
